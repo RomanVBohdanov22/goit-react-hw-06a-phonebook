cc972075ef591cf31790ea521d03c6bbcecf30bc
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2024-02-02 20:24:38 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,33 @@
-export const App = () => {
-  return (
-    <div
-      style={{
+function getRandomHexColor() {
+  return `#${Math.floor((0.2 + 0.5 * Math.random()) * 16777215).toString(16)}`;
+}
+
+const appStyles = {
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: '24px',
+  gap: '12px',
+  color: '#010101',
+};
+/**
+  style={{
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
+      }} 
+ */
+export const App = () => {
+  return (
+    <div
+      style={{
+        ...appStyles,
+        backgroundColor: getRandomHexColor(),
       }}
     >
       React homework template
